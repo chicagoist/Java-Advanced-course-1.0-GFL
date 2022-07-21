aeb24d714f16a685bfdcb979aa008ea32d434558
chicagoist <le.legioner@gmail.com>
2022-07-21 15:26:13 +0300

    hashCode & equals

--- ODTtast/src/com/task/taskODT/ODTask.java
+++ ODTtast/src/com/task/taskODT/ODTask.java
@@ -1,5 +1,9 @@
 package com.task.taskODT;
 
+//import java.lang.Override;
+
+import java.util.Objects;
+
 public class ODTask {
 
     private String word = new String();
@@ -10,33 +14,45 @@ public class ODTask {
         setWord("default");
         setID(0);
     }
+
     public ODTask(String args, Integer id) {
         this.word = args;
         this.id = id;
     }
 
 
-    public String getWord () {
+    public String getWord() {
 
         return this.word;
     }
 
-    public void setWord (String word) {
+    public void setWord(String word) {
 
         this.word = word;
     }
 
-    public Integer getID () {
+    public Integer getID() {
 
         return this.id;
     }
 
-    public void setID (Integer id) {
+    public void setID(Integer id) {
 
         this.id = id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ODTask odtask = (ODTask) o;
+        return Objects.equals(word, odtask.word) && Objects.equals(id, odtask.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, id);
+    }
 
 
 }
